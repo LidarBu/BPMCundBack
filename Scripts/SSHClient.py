2e4a4c319a063d880130dc3df2618eb14e4cb8d3
Lidar <lidarbuzaglo@gmail.com>
2021-10-06 09:28:38 +0200

    ssh

--- Scripts/SSHClient.py
+++ Scripts/SSHClient.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import paramiko
 from paramiko.ssh_exception import SSHException
@@ -5,7 +6,7 @@ from paramiko.ssh_exception import SSHException
 import Configuration
 
 
-class InitializeSSHClient:
+class SSHClient:
     connected = False
 
     def __init__(self):
@@ -22,7 +23,7 @@ class InitializeSSHClient:
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
-    def connect(self):
+    def __connect(self):
         if self.connected:
             return False, "The Client Already Connected"
 
@@ -30,10 +31,28 @@ class InitializeSSHClient:
             try:
                 self.ssh.connect(hostname=self.host_name, pkey=self.private_key)
                 self.connected = True
-                return False
+                return True
             except SSHException:
                 return False
 
+    def __disconnect(self):
+        if not self.connected:
+            return False, "The client is not connected"
+        else:
+            self.ssh.close()
+            self.connected = False
+            return True, "Disconnect the client"
+
     def execute_command(self, command):
+        self.__connect()
         ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(command)
+        self.__disconnect()
+        return ssh_stdin, ssh_stdout, ssh_stderr
+
+    def execute_script(self, path):
+        self.__connect()
+        with open(path, "r") as file:
+            script = json.load(file)
+        ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(script)
+        self.__disconnect()
         return ssh_stdin, ssh_stdout, ssh_stderr
--- Scripts/StartBPM.py
+++ Scripts/StartBPM.py
@@ -6,13 +6,11 @@ import os
 
 import paramiko
 
-from Scripts.InitializeSSHClient import InitializeSSHClient
+from Scripts.SSHClient import SSHClient
 
 
-
-def exe():
-    ssh_client = InitializeSSHClient()
-    ssh_client.connect()
-
-
+def exe(bpm_num):
+    dir_name = os.path.dirname(__file__)
+    path = os.path.join(dir_name, "../config/StartBPM.sh")
+    ssh_client = SSHClient()
 
